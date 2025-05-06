8a51496621e81e8dfc6118521aa12a3816aefd54
Venkata Santosh Kumar Sapireddy <sapireddysantosh.95@gmail.com>
2025-05-06 15:58:17 +0530

    saiteja

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! icloud '
+    return 'welcome'
+@app.route('/user')
+def user():
+    return 'sai teja'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
