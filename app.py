1ad7e22ee49eb589f3f89dc11a8224d50000346b
Venkata Santosh Kumar Sapireddy <sapireddysantosh.95@gmail.com>
2025-05-06 16:03:15 +0530

    santosh

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'welcome'
-@app.route('/user')
-def user():
-    return 'sai teja'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
