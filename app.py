2b95c3daafd90622f551eaffa06e6afb0918269f
Venkata Santosh Kumar Sapireddy <sapireddysantosh.95@gmail.com>
2025-05-07 11:00:21 +0530

    commit

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'santoash data'
+    return 'hello world'
+@app.route('/one')
+def one():
+    return render_template('one.html')
+@app.route('/two')
+def two():
+    return render_template('two.html')
+@app.route('/three')
+def three():
+    return render_template('three.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
