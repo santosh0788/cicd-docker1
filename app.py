b192baa21bf719486c4e847405579e19abb937e7
Venkata Santosh Kumar Sapireddy <sapireddysantosh.95@gmail.com>
2025-05-07 10:42:11 +0530

    update with the name

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! icloud'
+    return 'santoash data'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
